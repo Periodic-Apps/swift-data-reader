32b2ab179a80705c35f42a71aef8ea96ca013ec8
Matt Gadda <mgadda@gmail.com>
2022-12-25 14:04:53 -0800

    remove minimum OS requirements in favor of method specific version hinting

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftDataReader",
-    platforms: [.iOS(.v11), .macOS(.v11)],
+    name: "SwiftDataReader",    
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
